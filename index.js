eb6a51e03a687f79f852d49be4a9a3da5bf12b3e
Chetan <you@example.com>
2022-06-19 00:14:22 +0530

    '2nd'

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict;';
+const nam = document.querySelector('.input1');
+const password = document.querySelector('.input2');
+const form = document.querySelectorAll('#form');
+const btn = document.querySelectorAll('#submit');
+const usernam = document.querySelector('.username');
+const passwod = document.querySelector('.passwd');
+
+btn.addEventListener('click', (e) => {
+    if (nam.value === '') {
+        e.preventDefault();
+        usernam.classList.remove('Username');
+    }
+
+    if (password.value.length <= 4 && password.value.length > 10) {
+        e.preventDefault();
+        passwod.classList.remove('passwd');
+    }
+});
